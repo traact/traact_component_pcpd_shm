da2f1556589c1d7705ad3f11bf5cf50b88db9a54
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-06-20 17:18:36 +0200

    set conan info

--- conanfile.py
+++ conanfile.py
@@ -7,23 +7,26 @@ class TraactPackage(ConanFile):
     python_requires = "traact_run_env/1.0.0@traact/latest"
     python_requires_extend = "traact_run_env.TraactPackageCmake"
 
-    name = "your_component_name"
-    description = "..."
-    url = ""
-    license = ""
-    author = ""
+    name = "PCPD SHM client for traact"
+    description = "Source and Sink components using pcpd shared memory"
+    url = "https://github.com/traact/traact_component_pcpd_shm.git"
+    license = "MIT"
+    author = "Frieder Pankratz"
 
     settings = "os", "compiler", "build_type", "arch"
     compiler = "cppstd"
-
-    def _options(self):
-        self.options["with_bodytracking"] = [True, False]
-        self.default_options["with_bodytracking"] = True
 
     exports_sources = "src/*", "CMakeLists.txt"
 
     def requirements(self):
         # add your dependencies
+        self.traact_requires("traact_spatial", "latest")
         self.traact_requires("traact_vision", "latest")
+        self.requires("pcpd_shm_client/0.0.2@artekmed/stable")
         if self.options.with_tests:
             self.requires("gtest/[>=1.11.0]")
+
+    def configure(self):
+        self.options['pcpd_shm_client'].with_python = False
+        self.options['pcpd_shm_client'].with_visualization = False
+        self.options['pcpd_shm_client'].with_apps = False
